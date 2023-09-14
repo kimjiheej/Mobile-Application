e7b081f8d3902ee6270d94cf012330cd84f3e666
jay_010304 <glp0509@naver.com>
2023-09-14 14:06:06 +0900

    SQLiteDatabase 과제

--- app/src/main/java/ddwu/com/mobile/fooddbexam/RemoveActivity.kt
+++ app/src/main/java/ddwu/com/mobile/fooddbexam/RemoveActivity.kt
@@ -1,16 +1,46 @@
 package ddwu.com.mobile.fooddbexam
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import ddwu.com.mobile.fooddbexam.databinding.ActivityRemoveBinding
 
 class RemoveActivity : AppCompatActivity() {
 
-    lateinit var removeBinding : ActivityRemoveBinding
+    lateinit var removeBinding: ActivityRemoveBinding
+    lateinit var helper: FoodDBHelper
 
+    val TAG = "MainActivity"
+
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         removeBinding = ActivityRemoveBinding.inflate(layoutInflater)
         setContentView(removeBinding.root)
+
+
+        helper = FoodDBHelper(this)
+
+        removeBinding.btnRemoveFood.setOnClickListener {
+            val db = helper.writableDatabase
+            val whereClause = "food=?"
+            val whereArgs = arrayOf(removeBinding.etRemoveFood.text.toString())
+            db.delete("${FoodDBHelper.TABLE_NAME}", whereClause, whereArgs)
+            helper.close()
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        removeBinding.btnRemoveCancel.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        }
     }
-}
+
